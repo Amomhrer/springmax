0a24686f4b958d1bbc340c29d5e21d940c1b9504
Maksat <max.ast.kz@gmail.com>
2024-05-01 19:57:08 +0500

    Initial commit

---
+++ src/test/java/com/example/springmax/SpringmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
